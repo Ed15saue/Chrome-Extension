9d4e3c47aaa716e06c0363fb0ef853004f1e7fba
Eric Sauer <31904448+Ed15saue@users.noreply.github.com>
2019-09-16 12:28:26 -0400

    Add files via upload
    Adds the js file that determines which elements on the webpage to hide

---
+++ hideElement.js
@@ -0,0 +1,12 @@
+//gets the element that holds the url to the site
+var site = document.getElementsByClassName("iUh30 bc").textContent;
+var entry;
+//gets all elements with the url on the page and then hides them
+var block1 = site[0];
+console.log(block1);
+//for (var i = 0; i < site.length; i++){
+//    if(block == true){
+//       entry = site[0].parentElement.parentElement.parentElement.parentElement;
+//       entry.style.display = "none";
+//    }
+//}
